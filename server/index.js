8ba53eaf3d7ed763698d608d5bfba48a3eb7d371
Sihem Meriki <sihemmeriki2@gmail.com>
2019-09-25 15:44:21 +0200

    public

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../dist/index.html"));
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 }); // Send index.html for any other requests
 
 // error handling middleware
