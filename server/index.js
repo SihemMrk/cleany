033258eeeb5e38032272015ad1726bf33fd54f7f
Sihem Meriki <sihemmeriki2@gmail.com>
2019-09-25 15:56:30 +0200

    webpack config

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const path = require("path");
 const volleyball = require("volleyball");
 const app = express();
-const fs = require("fs");
+// const fs = require("fs");
 
 // logging middleware
 app.use(volleyball);
