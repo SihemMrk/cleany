88877b549ceb25de624a0c0777f4ab1d8220aed7
Sihem Meriki <sihemmeriki2@gmail.com>
2019-09-25 17:11:14 +0200

    page

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Page from "./components/page.js";
 
 const App = () => {
   return (
     <div>
-      <h1>Test</h1>
+      <Page />
     </div>
   );
 };
---
+++ src/components/page.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default class Page extends React.Component {
+  constructor() {
+    super();
+    // this.state = {
+    //   words: {}
+    // };
+  }
+  render() {
+    return (
+      <div>
+        <h1>Chercher un mot</h1>
+      </div>
+    );
+  }
+}
