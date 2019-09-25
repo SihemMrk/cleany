e11b0a66745195dea810bbf676e9c4ee46545d31
Sihem Meriki <sihemmeriki2@gmail.com>
2019-09-25 15:27:39 +0200

    setting up

---
+++ server/index.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const express = require("express");
+const path = require("path");
+const volleyball = require("volleyball");
+const app = express();
+const fs = require("fs");
+
+// logging middleware
+app.use(volleyball);
+
+// body parsing middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// static middleware
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
+}); // Send index.html for any other requests
+
+// error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || "Internal server error");
+});
+
+var port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log("Listening on port " + port);
+});
+
+module.exports = app;
